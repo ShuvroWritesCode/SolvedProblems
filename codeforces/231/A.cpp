91d7d53219b5b45aec94d6de1e042eb470a460fd
ShuvroWritesCode <shuvrohawk@gmail.com>
2021-09-10 12:59:00 +0600

    Add solution for problem `A - Team` Link: https://codeforces.com/contest/231/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/231/submission/128397865

--- codeforces/231/A.cpp
+++ codeforces/231/A.cpp
@@ -1,18 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-int32_t main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int a, b, c, n, ans; 
-	cin >> n;
-	ans = 0;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b >> c;
-		if(a+b+c > 1){
-			ans += 1;
-		}
-	}
-	cout << ans << '\n';
-	return 0;
+int main() {
+    int n, p, v, t, r;
+    r=0;
+    scanf("%d", &n);
+    while(n--) {
+        scanf("%d%d%d", &p, &v, &t);
+        r+=(p+v+t)/2;
+    }
+    printf("%d", r);
+    return 0;
 }
