2fc9a5a266ae2fb0a82f8b7b1004d4a3f51ca0e4
ShuvroWritesCode <shuvrohawk@gmail.com>
2021-10-10 17:00:00 +0600

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/131451471

--- codeforces/50/A.cpp
+++ codeforces/50/A.cpp
@@ -1,11 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int32_t main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int a, b;
-	cin >> a >> b;
-	cout << floor((a*b)/2) << '\n';
-	return 0;
+#include<stdio.h>
+int main()
+{
+    int a,b,x;
+scanf("%d %d",&a,&b);
+ 
+if(a>=1 && b<=16 ){
+    x=(a*b)/2;
+    printf("%d",x);
+}
+return 0;
 }
