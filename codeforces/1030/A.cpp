77f05acec7ab72bceda4958232ff2f24c6a91fc5
ShuvroWritesCode <shuvrohawk@gmail.com>
2021-10-11 19:57:00 +0600

    Add solution for problem `A - In Search of an Easy Problem` Link: https://codeforces.com/contest/1030/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1030/submission/131558965

--- codeforces/1030/A.cpp
+++ codeforces/1030/A.cpp
@@ -1,26 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
 
-int32_t main(){
-	#ifndef ONLINE_JUDGE
-		freopen("inputf.txt", "r", stdin);
-		freopen("output1.txt", "w", stdout);
-	#endif
-
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n, a[100]; cin >> n;
-	int sum = 0;
-	for(int i = 0; i < n; i++){
-		cin >> a[i];
-		sum += a[i];
-	}
-	if(sum > 0){
-		cout << "HARD";
-	}
-	else{
-		cout << "EASY";
-	}
-	return 0;
+int main(){
+    int opinion, sum;
+    scanf("%d", &opinion);
+    int arr[opinion], i;
+    for(i=0; i<opinion; i++){
+        scanf("%d ", &arr[i]);
+        sum += arr[i];
+    }
+    if(sum == 0){
+        printf("EASY\n");
+    }
+    else printf("HARD\n");
+    return 0;
 }
