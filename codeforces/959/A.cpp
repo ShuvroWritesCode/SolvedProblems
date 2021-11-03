0b70ed283c1296706f24b512190f878890433c67
ShuvroWritesCode <shuvrohawk@gmail.com>
2021-11-03 12:37:00 +0600

    Add solution for problem `A - Mahmoud and Ehab and the even-odd game` Link: https://codeforces.com/contest/959/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/959/submission/134181900

---
+++ codeforces/959/A.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t, n;
+    scanf("%d", &n);
+    if(n%2 == 0)
+    {
+        printf("Mahmoud");
+    }
+    else
+    {
+        printf("Ehab");
+    }
+    return 0;
+}
