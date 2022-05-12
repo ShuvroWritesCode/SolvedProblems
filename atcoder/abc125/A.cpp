bd73ad00953c0af0a15d87d9263170ecc9817f9b
ShuvroWritesCode <shuvrohawk@gmail.com>
2022-05-12 13:21:00 +0600

    Add solution for problem `A - Biscuit Generator` Link: https://atcoder.jp/contests/abc125/tasks/abc125_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc125/submissions/31617185

--- atcoder/abc125/A.cpp
+++ atcoder/abc125/A.cpp
@@ -1,10 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int32_t main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int A, B, T; cin >> A >> B >> T;
-	cout << (T/A)*B << '\n';
+int32_t main() {
+	int a, b, t;
+	cin >> a >> b >> t;
+	cout << (t/a)*b << '\n';
 	return 0;
 }
