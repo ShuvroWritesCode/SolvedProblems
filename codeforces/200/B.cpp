ea0524fdee08966d7d35521ed3698bb2d287aadf
ShuvroWritesCode <shuvrohawk@gmail.com>
2022-05-15 09:03:00 +0600

    Add solution for problem `B - Drinks` Link: https://codeforces.com/contest/200/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/200/submission/157254512

--- codeforces/200/B.cpp
+++ codeforces/200/B.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int32_t main(){
-
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	int n; cin >> n;
 	int a[n];
-	double amount;
 	for (int i = 0; i < n; i++){
 		cin  >> a[i];
+	}
+	double amount = 0;
+	for (int i = 0; i < n; i++){
 		amount += a[i];
 	}
-	cout << fixed << setprecision(10) << (double)(amount*100)/(n*100) << '\n';
+	cout << fixed << setprecision(10) << (double)(amount/(n*100))*100 << '\n';
 }
